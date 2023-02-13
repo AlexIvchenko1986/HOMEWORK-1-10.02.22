137c036414e64f228fa987ef5a9254b842a3b30d
Alexander Ivchenko <alex1986br@yandex.ru>
2023-02-13 14:07:12 +0300

    TASK2 MAX from 3

---
+++ TASK2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+int a;
+a = 25;      
+int b;
+b = 70;
+        
+int max;
+int min;
+        
+if (a > b)
+        {
+            max = b;
+            min = a;
+        }
+        else
+        {
+            max = a;
+            min = b;
+        }
+        Console.WriteLine(min);
+        Console.WriteLine(max);
