7d13ad34dea0d18eebcf79df4b0d78eff80d6002
Alexander Ivchenko <alex1986br@yandex.ru>
2023-02-13 14:09:25 +0300

    TASK4

---
+++ TASK4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+
+int a, b, c;
+
+a = 15;
+b = 20;
+c = 35;
+        
+int max;
+        
+max = a;
+if (b > max)
+{
+  max = b;
+    if (c > max)
+      {
+       max = c;
+       Console.WriteLine(max);
+      }
+      else
+      {
+        max = b;
+        Console.WriteLine(max);
+      }
+        }
+        else
+        {
+          max = a;
+          Console.WriteLine(max);
+        }
